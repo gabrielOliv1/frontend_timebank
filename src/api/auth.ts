dfa906e94c5b451ef9d9a68898e943d2d5eaf0c9
gabriel <gabrieloliveiravicente2015@gmail.com>
2025-03-24 17:31:45 -0300

    fix: changing api_url

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL =  process.env.API_URL
+const API_URL =  'https://timebank-a9encpbdeuejaefj.brazilsouth-01.azurewebsites.net'
 
 export const signupUser = async (name: string, email: string, password: string) => {
     try {
